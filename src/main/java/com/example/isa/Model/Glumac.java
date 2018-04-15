45809039072266cebc7c77238e110c568d1b2820
jovananovakovic <jovananovakovic404@gmail.com>
2018-04-15 20:35:14 +0200

    Model modifikovan

---
+++ src/main/java/com/example/isa/Model/Glumac.java
@@ -0,0 +1,48 @@
+package com.example.isa.Model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+public class Glumac implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private String ime;
+
+    @ManyToMany
+    @JsonIgnore
+    private List<Projekcija> projekcije;
+
+    public Glumac(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public List<Projekcija> getProjekcije() {
+        return projekcije;
+    }
+
+    public void setProjekcije(List<Projekcija> projekcije) {
+        this.projekcije = projekcije;
+    }
+}
